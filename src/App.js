e352ae0017ff14d0966292a58d5936610bfddaf5
Maria Dashkova <dashkova_m@inbox.ru>
2020-04-05 00:02:50 +0300

    add to git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './stylesheets/App.css';
+import VehicleList from './components/VehicleList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+        arrDate :[]}
+    }
+    componentWillMount() {
+        console.log("there");
+        fetch('https://raw.githubusercontent.com/denissokolov/tc-internship-task/master/launches.json')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({arrDate: data})
+            })
+            .catch(error => console.log('error', error));
+    }
+    render() {
+
+        return (
+            <div className="container-fluid">
+                <div>
+                    <h2>Welcome to SpaceX Transportation</h2>
+                    <p>Have a nice day)</p>
+                </div>
+                <VehicleList header="Missions" vehicles={this.state.arrDate}/>
+
+            </div>
+        )
+    }
 }
 
 export default App;
